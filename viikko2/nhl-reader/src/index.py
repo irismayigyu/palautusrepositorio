5a2463ce4f14ba550c289b29d38a63ae49b7bf1d
Iris <iris.perttuli@gmail.com>
2023-11-13 16:07:24 +0200

    pelaaja-tehtävät

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,24 @@
-from player import Player
+import requests
+from player import Player, PlayerReader, PlayerStats
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
+    # response = requests.get(url).json()
+
+    # print("JSON-muotoinen vastaus:")
+    # print(response)
+
+    # players = []
+
+    # for player_dict in response:
+    #     player = Player(player_dict)
+    #     players.append(player)
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,35 @@
+import requests
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.team = dict["team"]
+        self.goals = dict["goals"]
+        self.assists = dict["assists"]
+        self.points = self.assists + self.goals
     
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.team} {self.goals} + {self.assists} = {self.points}"
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        self.players = []
+        self.response=requests.get(self.url).json()
+
+        for player_dict in self.response:
+            player = Player(player_dict)
+            self.players.append(player)
+        return self.players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players=reader.get_players()
+        print("Suomalaiset pelaajat:")
+
+    def top_scorers_by_nationality(self,nat):
+        self.players = list(filter(lambda player: player.nationality == nat, self.players))
+        self.players = sorted(self.players, key=lambda player: player.points, reverse=True)
+        return self.players
