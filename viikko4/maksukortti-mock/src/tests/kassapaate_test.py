dcf7841d96c32018c03073c7bdaa23d625a45a48
Iris <iris.perttuli@gmail.com>
2023-11-27 18:46:43 +0200

    eka muutos

---
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import Mock, ANY
+from kassapaate import Kassapaate, HINTA
+from maksukortti import Maksukortti
+
+
+
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassa = Kassapaate()
+
+    def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+        
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_called_with(HINTA)
+
+    def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+        
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_not_called()
+
+    def test_ladatta_summa_pos(self):
+        maksukortti_mock = Mock()
+        maara=3
+        if maara>=0:
+            maksukortti_mock.lataa(maara)
+        else:
+            return
